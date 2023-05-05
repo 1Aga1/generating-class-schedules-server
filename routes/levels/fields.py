dc88fb90e46ba9e4036f680a64f50b501e19c360
Danil <Dpopov25@yandex.ru>
2023-05-05 23:26:32 +0500

    create level service

---
+++ routes/levels/fields.py
@@ -0,0 +1,15 @@
+from webargs import fields
+
+
+create_level_model = {
+    'text': fields.String(required=True)
+}
+
+delete_level_model = {
+    'id': fields.Number(required=True)
+}
+
+edit_level_model = {
+    'id': fields.Number(required=True),
+    'text': fields.String(required=True)
+}
--- routes/levels/levels.py
+++ routes/levels/levels.py
@@ -1,5 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, Response
 from webargs import flaskparser
+from .fields import create_level_model, delete_level_model, edit_level_model
+from service import levels_service
 
 level_router = Blueprint('level', __name__)
+
+
+@level_router.post('/level/create')
+def create():
+    data = flaskparser.parser.parse(create_level_model, request)
+    level = levels_service.create(data['text'])
+    return jsonify(level)
+
+
+@level_router.delete('/level/remove')
+def remove():
+    data = flaskparser.parser.parse(delete_level_model, request)
+    levels_service.delete(data['id'])
+    return Response(status=204)
+
+
+@level_router.post('/level/edit')
+def edit():
+    data = flaskparser.parser.parse(edit_level_model, request)
+    level = levels_service.edit(data['id'], data['text'])
+    return jsonify(level)
+
+
+@level_router.get('/levels')
+def get_levels():
+    levels = levels_service.get()
+    return jsonify(levels)
 
