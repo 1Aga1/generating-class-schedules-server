d6b6d90826086c0a0fd8ec02a92a3f4a448efafb
Danil <Dpopov25@yandex.ru>
2023-05-16 00:47:52 +0500

    document generation

--- routes/__init__.py
+++ routes/__init__.py
@@ -5,6 +5,7 @@ from .level_subjects import level_subjects
 from .schedules import schedules
 from .schedule_params import schedule_params
 from .user import user
+from .document import document
 
 routes = [
     levels.level_router,
@@ -13,5 +14,6 @@ routes = [
     level_subjects.level_subjects_router,
     schedules.schedules_router,
     schedule_params.schedule_params_router,
-    user.user_router
+    user.user_router,
+    document.document_router
 ]
