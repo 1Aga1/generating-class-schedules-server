9543650d3c1abb4e545076a36d1b885c9a25bf7a
Danil <Dpopov25@yandex.ru>
2023-06-03 21:02:59 +0500

    check office

--- service/schedule_params_service.py
+++ service/schedule_params_service.py
@@ -1,11 +1,25 @@
-from models import ScheduleParams, Subjects
+from models import ScheduleParams, Subjects, Schedules, Groups
 from exceptions import ApiError
 
 
 def add(schedule_id: int, group_id: int, subject_id: int, number: int):
-    schedule_param = ScheduleParams.select().where(schedule=schedule_id, subject=subject_id, number=number)
+    schedule = Schedules.get_or_none(id=schedule_id)
+    if not schedule:
+        raise ApiError.BadRequest('Schedule not found')
+
+    group = Groups.get_or_none(id=group_id)
+    if not group:
+        raise ApiError.BadRequest('Group not found')
+
+    subject = Subjects.get_or_none(id=subject_id)
+    if not subject:
+        raise ApiError.BadRequest('Subject not found')
+
+    schedule_param = ScheduleParams.select(ScheduleParams).join(Subjects)\
+        .where(ScheduleParams.schedule == schedule_id, Subjects.office == subject.office,
+               ScheduleParams.number == number)
     if schedule_param:
-        raise ApiError.BadRequest('Subject already set to another group')
+        raise ApiError.BadRequest('Office already set to another group')
 
     schedule_param = ScheduleParams(schedule=schedule_id, group=group_id, subject=subject_id, number=number)
     schedule_param.save()
