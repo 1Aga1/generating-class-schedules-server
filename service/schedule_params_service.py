9eddeea2f0a02c20a16b8c316017af0183d651f2
Danil <Dpopov25@yandex.ru>
2023-06-03 20:46:45 +0500

    Update schedule_params_service.py

--- service/schedule_params_service.py
+++ service/schedule_params_service.py
@@ -1,7 +1,12 @@
-from models import ScheduleParams
+from models import ScheduleParams, Subjects
+from exceptions import ApiError
 
 
 def add(schedule_id: int, group_id: int, subject_id: int, number: int):
+    schedule_param = ScheduleParams.select().where(schedule=schedule_id, subject=subject_id, number=number)
+    if schedule_param:
+        raise ApiError.BadRequest('Subject already set to another group')
+
     schedule_param = ScheduleParams(schedule=schedule_id, group=group_id, subject=subject_id, number=number)
     schedule_param.save()
     return schedule_param.get_dto()
